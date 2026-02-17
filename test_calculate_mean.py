29276d95b87e14c92e9088d58fadc03677759cd8
aysha.sparrowhawk1@yahoo.ca <ass740@uregina.ca>
2026-02-17 16:35:16 -0600

    Test file for calculate_mean function

---
+++ test_calculate_mean.py
@@ -0,0 +1,21 @@
+import pytest 
+from read_data import calculate_mean #import function that we want to test 
+
+#this is for the standard case when we have a list of numbers 
+def test_mean_standard():
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    #we assert (we ensure/state) that the mean of this list is above 5.5
+    #if function is implemented correctly, it will pass this test. 
+    #if the function is not implemented correctly, it will show is the error 
+    assert calculate_mean(numbers) == 5.5
+    
+#this is for the edge case when the list is empty
+def test_mean_empty():
+    # if the list is empty, our code should raise an error (called an exception)
+    with pytest.raises(ValueError):
+        calculate_mean([])
+
+def test_mean_string():
+    with pytest.raises(TypeError):
+        calculate_mean(str)
