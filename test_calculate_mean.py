7e1041e9868b3673d99cf608860143e56c93bf3b
aysha.sparrowhawk1@yahoo.ca <ass740@uregina.ca>
2026-02-20 18:40:05 -0600

    Changes made to error checking

--- test_calculate_mean.py
+++ test_calculate_mean.py
@@ -23,30 +23,43 @@ def test_mean_string():
     with pytest.raises(TypeError):
         calculate_mean(str)
         
-def test_fib_sequence():
-    num = [0, 1]
-    assert len(fibonacci_sequence(num)) >= 2
+
+def test_fib_str():
     with pytest.raises(TypeError):
         fibonacci_sequence(str)
+def test_fib_float():
     with pytest.raises(TypeError):
         fibonacci_sequence(float)
 
-def test_properly_balance():
+def test_balance_standard():
     sentence = "{}"
     with pytest.raises(TypeError):
+        len(properly_balanced(sentence)) % 2 != 0 #to ensure we enter an even number of characters. 
+        #cannot be properly balanced if we have an uneven amount of characters
+
+def test_balance_int():
+    with pytest.raises(TypeError):
         properly_balanced(int)
+        
+def test_balance_float():
     with pytest.raises(TypeError):
         properly_balanced(float)
-    assert len(properly_balanced(sentence)) % 2 == 0 #to ensure we enter an even number of characters. 
-    #cannot be properly balanced if we have an uneven amount of characters
 
-def test_merge_intervals():
-    ints = [[1, 2], [3, 4]]
-    for i in range(0, len(ints)):
-        left = ints[i][0]
-        right = ints[i][1]
+def test_ints_standard():
+    ints = [1, 2]
+    left = ints[0]
+    right = ints[1]
+    assert left <= right #need to make sure left endpoint of interval is smaller than right endpoint of interval
+    with pytest.raises(TypeError):
+        len(merge_intervals(ints)) %2 != 0 #ensures we have a left and right endpoint in each of the intervals
+        #I am beginning to think this is incorrect but I am going to leave it because I think it works?
+    
+def test_ints_empty():
+    with pytest.raises(ValueError):
+        merge_intervals([])
+
+def test_ints_str():
     with pytest.raises(TypeError):
         merge_intervals(str)
-    assert len(merge_intervals(ints[0])) %2 == 0 #ensures we have a left and right endpoint in each of the intervals
-    assert left <= right #need to make sure left endpoint of interval is smaller than right endpoint of interval
+    
     
